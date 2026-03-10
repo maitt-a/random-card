e1a0b2421de113aeb07b0dd5b171be91b1eb63bb
maitt-a <maiterocioalvarenga@gmail.com>
2026-03-10 11:40:05 -0300

    Mejora if

--- js/index.js
+++ js/index.js
@@ -3,15 +3,15 @@ let palos = ["♦︎","♥︎","♠︎","♣︎"];
 let numeros = ["A","2","3","4","5","6","Q","K"];
 
 // Funcion para obtener un elemento aleatorio
-function randomItem(array){
+function random(array){
     return array[Math.floor(Math.random()*array.length)];
 }
 
 // Colocación de los elementos en la function
-let palo = randomItem(palos);
-let numero = randomItem(numeros);
+let palo = random(palos);
+let numero = random(numeros);
 
-// Colocación de los elementos en el HTML
+// Colocación de los elementos en el HTML con DOM
 document.getElementById("top").innerHTML = palo;
 document.getElementById("bottom").innerHTML = palo;
 document.getElementById("number").innerHTML = numero;
@@ -19,4 +19,6 @@ document.getElementById("number").innerHTML = numero;
 // Colores
 if(palo === "♥︎" || palo === "♦︎"){
     document.querySelector(".card").style.color = "red";
+} else {
+    document.querySelector(".card").style.color = "black";
 }
